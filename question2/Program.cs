6459c1d33e9d900270864550279acaf9c4bbf1f9
selcancamsari <61784573+selcancamsari@users.noreply.github.com>
2024-03-27 22:24:15 +0300

    Add project files.

---
+++ question2/Program.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using question2;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        //veriyi oku ve stringe at.
+        string json = File.ReadAllText("C:\\Users\\Selcan\\source\\repos\\question2\\question2\\response.json");
+
+        //kontrol
+        if (json != null)
+        {
+            //deserialize et.
+            List<ReceiptModel> receiptModel = JsonConvert.DeserializeObject<List<ReceiptModel>>(json);
+
+            //çiftlemesin diye locale'leri kaldırdım.
+            var orderedReceiptModel = receiptModel.Where(s => s.locale == null).ToList();
+
+           var result = GetResult(orderedReceiptModel);
+
+            //Birden fazla gelmemesi için
+            foreach (var item in result.Distinct())
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Parse Edilen Json Formatı Hatalı ya da Boş!!");
+        }
+    }
+
+    //sonuçlarını getir
+    static List<string> GetResult(List<ReceiptModel> model)
+    {
+        List<string> result = new List<string>();
+        foreach (var item in model)
+        {
+            //tüm liste içinde koordinatlara bakarak kimin yukarıda kimin aşağıda ya da kimin sağda kimin solda olduğunu bulabilmek için.
+            var a = model.Where(s => (
+            ((s.boundingPoly.maxY + s.boundingPoly.minY) / 2 < item.boundingPoly.maxY)
+            &&
+            ((s.boundingPoly.maxY + s.boundingPoly.minY) / 2 > item.boundingPoly.minY)
+            ))
+            .OrderBy(x => x.boundingPoly.vertices[0].x).ToList().Select(m => m.description);
+
+            //Elemanları sıra ile getirmek için.
+            //Elemanların yanlarına boşluk eklemek için.
+            StringBuilder b = new StringBuilder();
+            foreach (var it in a)
+            {
+                b.Append(it);
+                b.Append(" ");
+            }
+
+            result.Add(b.ToString());
+        }
+
+        return result;
+
+    }
+}
+
+
+
---
+++ question2/ReceiptModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question2
+{
+    public class ReceiptModel
+    {
+        public string locale { get; set; }
+        public string description { get; set; }
+        public BoundingPoly? boundingPoly { get; set; }
+
+    }
+
+    public class BoundingPoly
+    {
+        public List<Vertices> vertices { get; set; }
+
+        //konumu için
+        public int minY
+        {
+            get { return vertices.Min(s => s.y); }
+        }
+        public int? maxY 
+        {
+            get { return vertices.Max(s => s.y); }
+        }
+
+        //0 sol üst 
+        //1 sağ üst 
+        //2 sağ alt
+        //3 sol alt
+    }
+
+    public class Vertices
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
